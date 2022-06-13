47208e34a44f073b4c3db0c8401ad8473d523ef2
Jerry Chen <jerryq.chen@gmail.com>
2022-06-14 02:31:32 +0800

    added mobile responsiveness to footer

--- movie-titles/src/Components/Footer/Footer.js
+++ movie-titles/src/Components/Footer/Footer.js
@@ -6,27 +6,29 @@ export default function Footer() {
   return (
     <footer className="page-footer font-small blue p-4 text-center">
       <main className="ms-5 me-5">
-        <section className="footerHeader d-flex flex-column ">
-          <span>Home | </span>
-          <span>Terms and Conditions | </span>
-          <span>Privacy Policy | </span>
-          <span>Collection Statement | </span>
-          <span>Help | </span>
+        <section className="footerHeader d-flex flex-column flex-md-row">
+          <span>Home |</span>
+          <span>Terms and Conditions |</span>
+          <span>Privacy Policy |</span>
+          <span>Collection Statement |</span>
+          <span>Help |</span>
           <span>Manage Account</span>
         </section>
 
-        <section className="mt-3 copyRight">
+        <section className="mt-3 copyRight d-flex flex-md-row">
           <p>Copyright @2016 DEMO Streaming All Rights Reserved. </p>
         </section>
 
-        <section className="text-white">
-          <i className="socialMediaIcon fa-brands fa-facebook-f"></i>
-          <i className="socialMediaIcon fa-brands fa-twitter"></i>
-          <i className="socialMediaIcon fa-brands fa-instagram-square"></i>
-        </section>
+        <section className="d-flex flex-column flex-md-row justify-content-md-between align-items-md-center">
+          <section className="text-white">
+            <i className="socialMediaIcon fa-brands fa-facebook-f"></i>
+            <i className="socialMediaIcon fa-brands fa-twitter"></i>
+            <i className="socialMediaIcon fa-brands fa-instagram-square"></i>
+          </section>
 
-        <section className="app store">
-          <img src={AppStore} alt="appStore" className="appStoreImg" />
+          <section className="app store">
+            <img src={AppStore} alt="appStore" className="appStoreImg" />
+          </section>
         </section>
       </main>
     </footer>
