01f225901e8600724cf0aa639057c1523915d3af
Jerry Chen <jerryq.chen@gmail.com>
2022-06-13 18:01:38 +0800

    added basic footer structure in mobile format

--- movie-titles/src/Components/Footer/Footer.js
+++ movie-titles/src/Components/Footer/Footer.js
@@ -1,5 +1,34 @@
 import React from "react";
+import "./style.css";
+import AppStore from "../../Images/appStore.png";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="page-footer font-small blue p-4 text-center">
+      <main className="ms-5 me-5">
+        <section className="footerHeader d-flex flex-column ">
+          <span>Home</span>
+          <span>Terms and Conditions</span>
+          <span>Privacy Policy</span>
+          <span>Collection</span>
+          <span>Help</span>
+          <span>Manage Account</span>
+        </section>
+
+        <section className="mt-3">
+          <p>Copyright @2022 DEMO Streaming. All rights reserved. </p>
+        </section>
+
+        <section className="text-white">
+          <i className="socialMediaIcon fa-brands fa-facebook-f"></i>
+          <i className="socialMediaIcon fa-brands fa-twitter"></i>
+          <i className="socialMediaIcon fa-brands fa-instagram-square"></i>
+        </section>
+
+        <section className="app store">
+          <img src={AppStore} alt="appStore" className="appStoreImg" />
+        </section>
+      </main>
+    </footer>
+  );
 }
